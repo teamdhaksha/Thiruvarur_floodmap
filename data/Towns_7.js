591f5858ed89ce519ea8a22dea2365a5eba06933
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-28 13:02:34 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,14 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Thiruvarur" }, "geometry": { "type": "Point", "coordinates": [ 79.63938935743154, 10.77503136998727, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Mannargudi" }, "geometry": { "type": "Point", "coordinates": [ 79.44696437309257, 10.66640259309292, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Muthupet" }, "geometry": { "type": "Point", "coordinates": [ 79.492991392194995, 10.39673463814483, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thiruthuraipoondi" }, "geometry": { "type": "Point", "coordinates": [ 79.637300675316752, 10.53327133409517, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kudavasal" }, "geometry": { "type": "Point", "coordinates": [ 79.481867887318174, 10.85912374592988, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Nannilam" }, "geometry": { "type": "Point", "coordinates": [ 79.615070783254367, 10.87928220828438, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Alangudi" }, "geometry": { "type": "Point", "coordinates": [ 79.405611360195678, 10.830403997974949, -999999.0 ] } }
+]
+}
